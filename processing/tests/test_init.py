ef622e2f674326221a867c9d5ca6bc56705f5923
Edvard Lindelöf <edvardlindelof@gmail.com>
2025-10-09 14:56:03 +0200

    placeholder test suite

---
+++ processing/tests/test_init.py
@@ -0,0 +1,9 @@
+"""Placeholder tests for processing package."""
+
+import pytest
+
+
+def test_import():
+    """Test that the package can be imported."""
+    import processing
+    assert processing.__version__
